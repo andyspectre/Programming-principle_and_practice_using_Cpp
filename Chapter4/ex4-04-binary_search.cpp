8db86b01df3bc3a6acf5e8dec904bf4e941d4ae0
Andrea Cocco <coccoa@pm.me>
2021-04-20 20:16:19 +0200

    Update ex4-04-binary_search.cpp

--- Chapter4/ex4-04-binary_search.cpp
+++ Chapter4/ex4-04-binary_search.cpp
@@ -1,9 +1,23 @@
+// Andrea Cocco 2020 
+// Chapter 4 exercise 4 from the book:
+// Programming: principles and practice using C++, 2nd edition
+// by Bjarne Stroustrup
+// https://www.stroustrup.com/programming.html
+
+/*
+        Write a program to play a numbers guessing game. The user thinks 
+        of a number between 1 and 100 and your program asks questions to 
+        figure out what the number is (e.g., "Is the number you are thinking
+        of less than 50?"). Your program should be able to identify the number 
+        after asking no more than seven questions.
+*/
+
 #include<iostream>
 
 int main()
 {
 /*
- *      The problem is to guess the number asking no more than seven
+ *      The problem is to guess the number after asking no more than seven
  *      questions. To do that, we need to use the binary search algorithm.
 */
         int min = 1;
