3b9f048e870e71144befe9fba204886143485726
Andrea Cocco <coccoa@pm.me>
2021-04-20 18:23:12 +0200

    Create ex3-3-compiler_reaction.cpp

---
+++ Chapter3/ex3-3-compiler_reaction.cpp
@@ -0,0 +1,17 @@
+// Andrea Cocco 2020
+// Chapter 3 Exercise 3
+/*      
+ *      Write a program that doesn't do anything, but declares a 
+ *      number of variables with legal and illegal names, so that
+ *      you can see how the compiler reacts.
+*/
+
+#include<iostream>
+
+int main()
+{
+        char a = ' ';
+        bool if = true;
+        int x = 0;
+        double int = 0;
+}
