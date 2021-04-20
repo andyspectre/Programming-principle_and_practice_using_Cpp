cca9b494962042b194cf39c8480c7c5bd3efbaa4
Andrea Cocco <coccoa@pm.me>
2021-04-20 19:31:16 +0200

    Update ex4-02-median_temp.cpp

--- Chapter4/ex4-02-median_temp.cpp
+++ Chapter4/ex4-02-median_temp.cpp
@@ -1,3 +1,14 @@
+// Andrea Cocco 2020 
+// Chapter 4 exercise 2 from the book:
+// Programming: principles and practice using C++, 2nd edition
+// by Bjarne Stroustrup
+// https://www.stroustrup.com/programming.html
+
+/*
+        A program that prompts the user to enter some temperature values
+        and then prints the average and the median temperature.
+*/
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -5,7 +16,8 @@
 int main()
 {
         std::vector<double> temps;
-        std::cout << "Enter some temperatures: ";
+        std::cout << "Enter some temperatures"
+                << " (enter '|' to print the result): ";
         for (double t; std::cin >> t;) {
                 temps.push_back(t);
         }
@@ -19,7 +31,8 @@ int main()
         std::sort(temps.begin(),temps.end());
 
         if (temps.size()%2 != 0) {
-                std::cout << "Median temperature: " << temps[temps.size()/2] << '\n';
+                std::cout << "Median temperature: " 
+                        << temps[temps.size()/2] << '\n';
         }
         else {
                 double second_midnum = temps[temps.size()/2];
@@ -28,5 +41,3 @@ int main()
                 std::cout << "Median temperature: " << median << '\n';
         }       
 }
-
-                
