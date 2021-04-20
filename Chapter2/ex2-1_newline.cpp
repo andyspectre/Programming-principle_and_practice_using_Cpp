e4b8e43aa64b1c9d9962dbdf209c105b8c2df5c5
Andrea Cocco <coccoa@pm.me>
2021-04-20 17:26:32 +0200

    Update ex2-1_newline.cpp

--- Chapter2/ex2-1_newline.cpp
+++ Chapter2/ex2-1_newline.cpp
@@ -1,20 +1,20 @@
 // Andrea Cocco 2020 
 // Chapter 2 Exercise 1 from the book:
-
+//
 // Programming: principles and practice using C++, 2nd edition
 // by Bjarne Stroustrup
 // https://www.stroustrup.com/programming.html
 
 /*
-	Change the program to output the two lines
-		Hello, programming!
-		Here we go!
+        Change the program to output the two lines
+        Hello, programming!
+        Here we go!
 */
 
 #include<iostream>
 
 int main()
 {
-	std::cout << "Hello, programming!\nHere we go!\n";
-	return 0;
+        std::cout << "Hello, programming!\nHere we go!\n";
+        return 0;
 }
