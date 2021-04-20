5276f9be2e97947651f79c31bde9c62cb916d3bb
Andrea Cocco <coccoa@pm.me>
2021-04-20 17:58:47 +0200

    Update ex2-1_newline.cpp

--- Chapter2/ex2-1_newline.cpp
+++ Chapter2/ex2-1_newline.cpp
@@ -1,6 +1,5 @@
 // Andrea Cocco 2020 
 // Chapter 2 Exercise 1 from the book:
-//
 // Programming: principles and practice using C++, 2nd edition
 // by Bjarne Stroustrup
 // https://www.stroustrup.com/programming.html
