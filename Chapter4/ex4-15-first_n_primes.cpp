0b8bde2b90058f24da303e0a0aec3a77dd3f229c
Andrea Cocco <coccoa@pm.me>
2021-04-21 13:07:45 +0200

    Create ex4-15-first_n_primes.cpp

---
+++ Chapter4/ex4-15-first_n_primes.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+#include<cmath>
+
+std::vector<int> trial_div(int n)
+/*
+        Trial division algorithm, optimized by first checking for 
+        divisibility by 2 and then starting trial division from 3, 
+        checking only odd numbers.
+*/
+{
+        bool is_prime = true;
+        int x = 3;        // numbers that will be tested for primality
+        std::vector<int> primes;
+
+        if (n >= 1) {
+                primes.push_back(2);        // 2 is always the first prime :)
+        }
+
+        if (n > 1) {
+                for (int i = 2; i <= n;) {        // find n prime numbers
+                        // if x is divisible by 2 is not prime
+                        if (x%2 == 0) {        
+                                is_prime = false;
+                        }
+                        /*
+                                Since we already checked if x is divisible by 2,
+                                we only need to check for odd numbers, 
+                                up to the sqrt of x. 
+                                If x is divisible (by an odd number), 
+                                then x is not prime
+                        */
+                        for (int k = 3; k <= sqrt(x); k+=2) { 
+                                if (x%k == 0) {               
+                                        is_prime = false;     
+                                        break;
+                                }
+                        }
+                        if (is_prime) { // x is prime
+                                primes.push_back(x);
+                                ++i;
+                        }
+                        is_prime = true;
+                        ++x;
+                }
+        }
+        return primes;
+}
+                                
+        
+int main()
+{
+        int n = 0;
+        std::cout << "Enter a number n to check first n prime numbers: ";
+        std::cin >> n;
+        std::vector<int> first_primes = trial_div(n);
+        for (int i : first_primes) {
+                std::cout << i << " ";
+        }
+}
