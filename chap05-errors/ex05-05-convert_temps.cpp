5710f83d1cc56ad695ca006bc2a27b615886bd9e
c0cc0a <coccoa@pm.me>
2021-05-12 11:34:58 +0200

    add exercise 05 to chapter 05

---
+++ chap05-errors/ex05-05-convert_temps.cpp
@@ -0,0 +1,67 @@
+// Andrea Cocco 2020 
+// Chapter 5 exercise 05 from the book:
+// Programming: principles and practice using C++, 2nd edition
+// by Bjarne Stroustrup
+// https://www.stroustrup.com/programming.html
+
+/*
+ *      Add to the program (n.03) so that it can also convert from 
+ *      Kelvin to Celsius.
+*/
+
+#include<iostream>
+
+
+void error(std::string s)
+{
+        throw std::runtime_error(s);
+}
+
+double ctok(double v, char u)
+/*      c is degrees in Celsius. The minimum value is -273.15
+ *      which is the absolute zero.
+*/
+{
+        constexpr double abs_zero = -273.15;
+        double converted_t = 0;
+
+        if (v < abs_zero) {
+                error("Value lower than absolute zero");
+        }
+        if (!(u == 'k' || u == 'K' || u == 'c' || u == 'C')) {
+                error("Unit of temperature not supported");
+        }
+
+        if (u == 'k' || u == 'K') { 
+                converted_t = v + abs_zero;
+        }
+        else {
+                converted_t = v - abs_zero;
+        }
+        return converted_t; 
+}
+
+int main()
+try {
+        std::cout << "Insert temperature value in Celsius [C]"
+        << " or Kelvin [K]: ";
+
+        double v = 0;
+        double convert_temps = 0;
+        char u = ' ';
+
+        std::cin >> v >> u; 
+        if (!std::cin) {
+                error("Invalid input");
+        }
+        else {
+                convert_temps = ctok(v, u);
+                std::cout << v << " degrees " << u 
+                        << " are " << convert_temps << '\n';
+        }
+        return 0;
+}
+catch (std::runtime_error& e) {
+        std::cerr << "runtime error: " << e.what() << '\n';
+        return 1;
+}
