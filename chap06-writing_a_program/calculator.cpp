8dc6b6449d40d95ebbf61bf434590cad35a65c07
c0cc0a <coccoa@pm.me>
2021-06-16 20:23:34 +0200

    defined class Token and class Token_stream

--- chap06-writing_a_program/calculator.cpp
+++ chap06-writing_a_program/calculator.cpp
@@ -4,25 +4,44 @@
 // by Bjarne Stroustrup
 // https://www.stroustrup.com/programming.html
 
-/* The program is a calculator that computes arithmetic expressions like:
+/*
+ *      The program is a calculator that computes arithmetic expressions like:
+ *      5+3*2-(9/3+3)
+ *      It accepts addition, subtraction, multiplication, division and 
+ *      parenthesis for grouping.
  *
- * 5+3*2-(9/3+3)
- * It accepts addition, subtraction, multiplication, division and parenthesis
- * for grouping.
+ *      Grammar defining the syntax of the input. Tokens are in double quotes.
+ *      Alternatives are put on separate lines:
  *
- * Grammar defining the syntax of the input. Tokens are in double quotes.
- * Alternatives are put on separate lines:
- *
- * Expression:
- *      Term
- *      Expression "+" Term     // addition
- *      Expression "-" Term     // subtraction
- * Term:
- *      Primary
- *      Term "*" Primary        // multiplication
- *      Term "/" Primary        // division
- * Primary:
- *      Number
- *      "(" Expression ")"      // grouping
- * Number:
- *      floating-point-literal
+ *      Expression:
+ *           Term
+ *           Expression "+" Term     // addition
+ *           Expression "-" Term     // subtraction
+ *      Term:
+ *           Primary
+ *           Term "*" Primary        // multiplication
+ *           Term "/" Primary        // division
+ *      Primary:
+ *           Number
+ *           "(" Expression ")"      // grouping
+ *      Number:
+ *           floating-point-literal
+*/ 
+
+#include<iostream>
+
+class Token {    // add something 
+public:
+        char kind;
+        double value;
+};
+
+class Token_stream {    // a "stream" of Tokens
+public:
+        Token get();    // get a Token
+        void putback(Token t);    // put a Token back in to the Token stream
+private:
+        bool full{false};    // is there a Token in the buffer?
+        Token buffer;        // here is where we keep a Token using putback()
+};
+
