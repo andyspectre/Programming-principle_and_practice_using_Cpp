ea0d1ad9bb9b48afe263a7faf43d5d1211adf74d
c0cc0a <coccoa@pm.me>
2021-08-25 19:32:09 +0200

    ex06-09: first steps

---
+++ chap06-writing_a_program/ex06-09-char_to_int.cpp
@@ -0,0 +1,31 @@
+// Andrea Cocco 2020 
+// Chapter 6 exercise 9 
+// Programming: principles and practice using C++, 2nd edition
+// by Bjarne Stroustrup
+// https://www.stroustrup.com/programming.html
+
+/*
+ *      Write a program that reads digits and composes them into integers.
+ *      For example, 123 is read as the characters 1, 2 and 3. The program
+ *      should output 123 is 1 hundred and 2 tens and 3 ones. The number
+ *      should be output as an int value. Handle numbers with one, two, three
+ *      or four digits.
+*/
+
+#include<iostream>
+
+using std::cin;
+using std::cout;
+
+int main()
+{
+        char n {' '};
+        constexpr char c {'0'};    // need this to convert char to int properly
+        int x {0};
+        
+        cin >> n;
+        x = n-c;    // subtract '0' to char for correct conversion to int
+        
+        cout << x << '\n';
+}
+        
