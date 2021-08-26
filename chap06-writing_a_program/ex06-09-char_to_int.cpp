05fc8002f7a3cd3a6b4ab39d268f343114e4b73f
c0cc0a <coccoa@pm.me>
2021-08-26 19:11:35 +0200

    main(): add the two main vectors; add input validation; add some scaffolding; add exception handler.

--- chap06-writing_a_program/ex06-09-char_to_int.cpp
+++ chap06-writing_a_program/ex06-09-char_to_int.cpp
@@ -13,20 +13,39 @@
 */
 
 #include<iostream>
+#include<vector>
 
 using std::cin;
 using std::cout;
+using std::vector;
+using std::string;
+using std::runtime_error;
+using std::cerr;
+
+void error(string s) {
+        throw runtime_error(s);
+}
 
 int main()
-{
+try{
         char n {' '};
         constexpr char c {'0'};    // need this to convert char to int properly
         int x {0};
-        
+        vector<int> digits;
+        vector<string> units {"ones", "tens", "hundreds", "thousands"};
+
         // input char and output int until quit with 'q'
         while (cin >> n && n != 'q') {
+                // input validation
+                if ((n < 'q' && n < '0') || (n > '9')) error("invalid input");
                 x = n-c;    // subtract '0' to char for correct conversion to int
-                cout << x << '\n';
+                digits.push_back(x);
         }
+        // scaffolding
+        for (int i : digits)
+                cout << i;
+                cout << '\n';
 }
-        
+catch(runtime_error e) {
+        cerr << e.what() << '\n';
+}
